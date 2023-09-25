c59605e2b221fe50ca98721fb3e37e7be4e812cc
özkan demir <ozkanndemir2009@gmail.com>
2023-09-25 23:17:39 +0300

    tailwind css added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      özkan
+      GKMUSTECH <br />
+      <p> BU BİR TESTER ŞİRKETİ OLUP SAYFAMIZ YAPIM AŞAMASINDADIR</p>
     </div>
   );
 }
